3df2ef4d66c03dcc9c294f3124305e50f82b4a95
hocaron <pooh0407@ewhain.net>
2022-06-01 18:37:26 +0900

    feat : core api 생성

---
+++ src/api/core.ts
@@ -0,0 +1,31 @@
+import axios, {AxiosInstance, AxiosRequestConfig, Method} from 'axios';
+import {HTTP_METHODS} from '../consts/net';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api/v1',
+  timeout: 10000,
+  headers: {'Content-Type': 'application/json'},
+});
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (config: AxiosRequestConfig): Promise<any> => {
+    _axiosInstance.interceptors.response.use(response => {
+      if (!response.data) return response;
+      return response.data.data;
+    });
+
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
