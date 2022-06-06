3876d2f901c388baabafabada6a21d35f87b0607
hocaron <pooh0407@ewhain.net>
2022-06-06 19:16:52 +0900

    chore : 메인페이지 라우터 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import {HOME_PAGE, JOIN_PAGE, LOGIN_PAGE, NOT_FOUND_PAGE} from './consts/route';
+import {HOME_PAGE, JOIN_PAGE, LOGIN_PAGE, MAIN_PAGE, NOT_FOUND_PAGE} from './consts/route';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Join from './pages/Join/Join';
+import Main from './pages/Main/Main';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path={HOME_PAGE} component={Home} />
         <Route path={LOGIN_PAGE} component={Login} />
         <Route path={JOIN_PAGE} component={Join} />
+        <Route path={MAIN_PAGE} component={Main} />
         <Redirect to={NOT_FOUND_PAGE} />
       </Switch>
     </>
