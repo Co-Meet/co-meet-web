c73abf5f5e2bda8586147b17aac2ac176d58b7ce
hocaron <pooh0407@ewhain.net>
2022-06-06 23:25:26 +0900

    feat : 토큰 유무로 분기처리하는 로직 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {QueryClient, QueryClientProvider} from 'react-query';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import {LoginProvider} from './contexts/LoginContext';
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <Router>
-      <LoginProvider>
-        <App />
-      </LoginProvider>
-    </Router>
-  </QueryClientProvider>,
+  <Router>
+    <LoginProvider>
+      <App />
+    </LoginProvider>
+  </Router>,
   document.getElementById('app'),
 );
