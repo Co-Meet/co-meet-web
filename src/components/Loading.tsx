814ccb7c8cf644ab0fa645acaa8bb359acb0f60a
hocaron <pooh0407@ewhain.net>
2022-06-06 19:17:55 +0900

    chore : 로딩 컴포넌트 추가

---
+++ src/components/Loading.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Spin} from 'antd';
+import styled from 'styled-components';
+import {LoadingOutlined} from '@ant-design/icons';
+
+const Loading: React.FC = () => {
+  return (
+    <Container>
+      <Spin tip="Loading..." indicator={<LoadingOutlined style={{fontSize: 50}} />} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Loading;
