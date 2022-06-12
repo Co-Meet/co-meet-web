2f7709f2aef3eb1cb8a630f43c343c47e1dbdab1
hocaron <kkannu0407@gmail.com>
2022-06-12 15:40:46 +0900

    feat : 모달창 생성

--- src/pages/OrganizationDetail/OrganizationDetail.tsx
+++ src/pages/OrganizationDetail/OrganizationDetail.tsx
@@ -1,15 +1,22 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import {Col, Row} from 'antd';
 import {Member} from '../../types/dto/member';
 import {useQuery} from 'react-query';
 import {getCommit} from '../../api/commit';
+import Modal from '../../components/OrganizationDetail/Modal';
 
 function OrganizationDetail() {
   const {state} = useLocation();
   const data: any = state;
   const memberList = data.state.members;
   let commitList: any = [];
+
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+
+  const onClickToggleModal = useCallback(() => {
+    setOpenModal(!isOpenModal);
+  }, [isOpenModal]);
 
   const handleMemberCommitInfo = useCallback(() => {
     commitList = memberList.map((member: Member) => {
@@ -22,6 +29,10 @@ function OrganizationDetail() {
     return (
       <Row gutter={[16, 16]} style={{marginTop: '30px'}}>
         {handleMemberCommitInfo()}
+        {isOpenModal && (
+          <Modal onClickToggleModal={onClickToggleModal}>이곳에 children이 들어갑니다.</Modal>
+        )}
+        <button onClick={onClickToggleModal}>Open Modal</button>
         {commitList.map((commit: any, idx: number) => (
           <Col key={commit.nickname} xs={24} md={12} lg={8}>
             <div>
@@ -33,6 +44,7 @@ function OrganizationDetail() {
       </Row>
     );
   }, []);
+
   return (
     <div>
       <h2 style={{fontWeight: 'bold'}}>오거니제이션 정보 보기</h2>
