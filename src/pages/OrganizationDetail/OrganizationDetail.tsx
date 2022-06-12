17ae3010cf9a4b5d4b2428ea5270810f64173380
hocaron <kkannu0407@gmail.com>
2022-06-13 00:04:27 +0900

    feat : 오거니제이션 상세페이지에 모달 적용

--- src/pages/OrganizationDetail/OrganizationDetail.tsx
+++ src/pages/OrganizationDetail/OrganizationDetail.tsx
@@ -8,19 +8,19 @@ import Modal from '../../components/OrganizationDetail/Modal';
 
 function OrganizationDetail() {
   const {state} = useLocation();
-  const data: any = state;
-  const memberList = data.state.members;
-  let commitList: any = [];
-
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
 
   const onClickToggleModal = useCallback(() => {
     setOpenModal(!isOpenModal);
   }, [isOpenModal]);
 
+  const data: any = state;
+  const memberList = data.state.members;
+  let commitList: any = [];
+
   const handleMemberCommitInfo = useCallback(() => {
     commitList = memberList.map((member: Member) => {
-      const {status, data} = useQuery(['organizations'], () => getCommit(member.id));
+      const {data} = useQuery(['organizations'], () => getCommit(member.id));
       return {nickname: member.nickname, commits: data?.commits};
     });
   }, []);
@@ -29,10 +29,6 @@ function OrganizationDetail() {
     return (
       <Row gutter={[16, 16]} style={{marginTop: '30px'}}>
         {handleMemberCommitInfo()}
-        {isOpenModal && (
-          <Modal onClickToggleModal={onClickToggleModal}>이곳에 children이 들어갑니다.</Modal>
-        )}
-        <button onClick={onClickToggleModal}>Open Modal</button>
         {commitList.map((commit: any, idx: number) => (
           <Col key={commit.nickname} xs={24} md={12} lg={8}>
             <div>
@@ -48,6 +44,10 @@ function OrganizationDetail() {
   return (
     <div>
       <h2 style={{fontWeight: 'bold'}}>오거니제이션 정보 보기</h2>
+      {isOpenModal && (
+        <Modal onClickToggleModal={onClickToggleModal}>이곳에 children이 들어갑니다.</Modal>
+      )}
+      <button onClick={onClickToggleModal}>Open Modal</button>
       {renderByStatus()}
     </div>
   );
