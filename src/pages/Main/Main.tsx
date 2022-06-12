2a32f0af9f6cf686749d56b1fc6589cd547aa1ed
hocaron <kkannu0407@gmail.com>
2022-06-13 02:35:09 +0900

    feat : 메인페이지에서 사용자 오거니제이션 삭제 로직 생성

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,20 @@
 import React, {useCallback} from 'react';
 import {useHistory} from 'react-router-dom';
-import {useQuery} from 'react-query';
+import {useMutation, useQuery} from 'react-query';
 import {getMyOrganization} from '../../api/member';
 import Loading from '../../components/Loading';
 import {Col, Row} from 'antd';
 import {Organization} from '../../types/dto/organization';
 import {Member} from '../../types/dto/member';
+import {removeMember} from '../../api/organization';
 
 function Main() {
   const history = useHistory();
   const {status, data} = useQuery(['organizations'], () => getMyOrganization());
+
+  const {mutate, isLoading, isError, error, isSuccess} = useMutation((id: number) =>
+    removeMember(id),
+  );
 
   const handleOrganizationInfo = useCallback(
     async (idx: any): Promise<void> => {
@@ -31,16 +36,24 @@ function Main() {
         return <Loading />;
       case 'success':
         const organizationList = data.organizationList;
-        const members = organizationList[0].members;
         return (
           <Row gutter={[16, 16]} style={{marginTop: '30px'}}>
             {organizationList.map((organization: Organization, idx: number) => (
               <Col key={organization.id} xs={24} md={12} lg={8}>
-                <div onClick={() => handleOrganizationInfo(idx)} key={organization.id}>
-                  <div>{organization.name}</div>
-                  {organization.members.map((member: Member, idx: number) => (
-                    <div>{member.nickname}</div>
-                  ))}
+                <div>
+                  <button
+                    onClick={() => {
+                      mutate(organization.id);
+                    }}
+                  >
+                    x
+                  </button>
+                  <div onClick={() => handleOrganizationInfo(idx)} key={organization.id}>
+                    <div>{organization.name}</div>
+                    {organization.members.map((member: Member, idx: number) => (
+                      <div>{member.nickname}</div>
+                    ))}
+                  </div>
                 </div>
               </Col>
             ))}
