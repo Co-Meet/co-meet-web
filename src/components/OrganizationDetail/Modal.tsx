19facf0ce62d24f62148b926c2707ac6b366d7a8
hocaron <kkannu0407@gmail.com>
2022-06-13 01:01:58 +0900

    feat : 오거니제이션 상세페이지에서 멤버 추가 로직 생성

--- src/components/OrganizationDetail/Modal.tsx
+++ src/components/OrganizationDetail/Modal.tsx
@@ -1,27 +1,89 @@
-import React, {PropsWithChildren} from 'react';
+import React, {PropsWithChildren, useCallback, useState} from 'react';
 import styled from 'styled-components';
+import {useMutation} from 'react-query';
+import {Button, Form, Input} from 'antd';
+import {AddMemberRequestData} from '../../types/dto/organization';
+import {addMember} from '../../api/organization';
 
 interface ModalDefaultType {
   onClickToggleModal: () => void;
 }
 
 function Modal({onClickToggleModal, children}: PropsWithChildren<ModalDefaultType>) {
+  const mutationAddMember = useMutation((addMemberRequestData: AddMemberRequestData) =>
+    addMember(children, addMemberRequestData),
+  );
+  const [addMemberRequestData, setAddMemberRequestData] = useState({nickname: ''});
+  const [errorMsg, setErrorMsg] = useState('');
+  const handleOnChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
+      setAddMemberRequestData({...addMemberRequestData, [e.target.name]: e.target.value});
+    },
+    [addMemberRequestData],
+  );
+
+  const handleAddMember = useCallback((): void => {
+    mutationAddMember
+      .mutateAsync(addMemberRequestData)
+      .then(res => {
+        if (res) {
+          if (onClickToggleModal) {
+            onClickToggleModal();
+          }
+        }
+      })
+      .catch(() => {
+        setErrorMsg('닉네임 조회에 실패했습니다');
+      });
+  }, [mutationAddMember, addMemberRequestData]);
+
   return (
     <ModalContainer>
       <button>x</button>
       <DialogBox>
-        {children}
-        <button
-          onClick={(e: React.MouseEvent) => {
-            e.preventDefault();
+        <Form onFinish={handleAddMember} autoComplete="off">
+          <Form.Item
+            label="닉네임"
+            name="nickname"
+            rules={[
+              {required: true, message: '닉네임을 입력해주세요'},
+              {
+                type: 'string',
+                message: '닉네임을 입력해주세요.',
+              },
+            ]}
+          >
+            <Input
+              name="nickname"
+              value={addMemberRequestData.nickname}
+              onChange={handleOnChange}
+              size="large"
+            />
+          </Form.Item>
 
-            if (onClickToggleModal) {
-              onClickToggleModal();
-            }
-          }}
-        >
-          x
-        </button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            disabled={mutationAddMember.isLoading}
+          >
+            로그인
+          </Button>
+
+          <p>{errorMsg}</p>
+
+          <button
+            onClick={(e: React.MouseEvent) => {
+              e.preventDefault();
+
+              if (onClickToggleModal) {
+                onClickToggleModal();
+              }
+            }}
+          >
+            x
+          </button>
+        </Form>
       </DialogBox>
       <Backdrop />
     </ModalContainer>
